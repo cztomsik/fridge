c3940b814791fa25065e128f696e3765bbcab9d3
Kamil Tomsik <info@tomsik.cz>
2024-03-19 14:51:12 +0100

    migrate: update to the new api

--- src/migrate.zig
+++ src/migrate.zig
@@ -3,52 +3,63 @@
 // different
 
 const std = @import("std");
-const sqlite = @import("sqlite.zig");
+const dsl = @import("dsl.zig");
+const SQLite3 = @import("sqlite.zig").SQLite3;
+const Session = @import("session.zig").Session;
 const log = std.log.scoped(.db_migrate);
 
-pub fn migrate(allocator: std.mem.Allocator, db: *sqlite.SQLite3, ddl: []const u8) !void {
+pub fn migrate(allocator: std.mem.Allocator, filename: [*:0]const u8, ddl: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    const mem = try SQLite3.open(":memory:");
+    var pristine = Session.fromConnection(arena.allocator(), mem);
+    defer pristine.deinit();
+
+    const conn = try SQLite3.open(filename);
+    var db = Session.fromConnection(arena.allocator(), conn);
+    defer db.deinit();
+
     // Make sure we're in WAL mode and synchronous
     // (this is important for data integrity)
-    try db.exec("PRAGMA journal_mode = WAL", .{});
-    try db.exec("PRAGMA synchronous = FULL", .{});
+    try db.exec("PRAGMA journal_mode = WAL");
+    try db.exec("PRAGMA synchronous = FULL");
 
     // Create empty database with the desired schema
     log.debug("-- Creating pristine database", .{});
-    var pristine = try sqlite.SQLite3.open(":memory:");
-    defer pristine.close();
-    try pristine.execAll(ddl);
+    try pristine.conn.execAll(ddl);
 
     // Start a transaction and disable foreign key checks
-    try db.exec("BEGIN", .{});
-    try db.exec("PRAGMA foreign_keys = OFF", .{});
+    try db.exec("BEGIN");
+    try db.exec("PRAGMA foreign_keys = OFF");
 
     // Migrate each object type
-    try migrateObjects(allocator, db, &pristine, "table");
-    try migrateObjects(allocator, db, &pristine, "view");
-    try migrateObjects(allocator, db, &pristine, "trigger");
-    try migrateObjects(allocator, db, &pristine, "index");
+    inline for (.{ "table", "view", "trigger", "index" }) |kind| {
+        try migrateObjects(&db, &pristine, kind);
+    }
 
     // Re-enable foreign key checks and commit
-    try db.exec("PRAGMA foreign_keys = ON", .{});
-    try db.exec("COMMIT", .{});
+    try db.exec("PRAGMA foreign_keys = ON");
+    try db.exec("COMMIT");
 }
 
-fn migrateObjects(allocator: std.mem.Allocator, db: *sqlite.SQLite3, pristine: *sqlite.SQLite3, kind: []const u8) !void {
-    var objects = try pristine.query("SELECT name, sql FROM sqlite_master WHERE type = ? AND name != 'sqlite_sequence' AND name NOT LIKE 'sqlite_autoindex_%'", .{kind});
-    defer objects.deinit();
+fn migrateObjects(db: *Session, pristine: *Session, kind: []const u8) !void {
+    const objects = dsl.query(sqlite_master)
+        .where(.{ .type = kind })
+        .where(dsl.raw("name != 'sqlite_sequence' AND name NOT LIKE 'sqlite_autoindex_%'", .{}));
 
-    while (try objects.readNext(struct { []const u8, []const u8 })) |row| {
-        log.debug("-- Checking {s} {s}", .{ kind, row[0] });
+    for (try pristine.findAll(objects)) |obj| {
+        log.debug("-- Checking {s} {s}", .{ kind, obj.name });
 
         // Check if object exists
-        if (!try db.get(bool, "SELECT COUNT(*) FROM sqlite_master WHERE type = ? AND name = ?", .{ kind, row[0] })) {
+        const curr = try db.findBy(sqlite_master, .{ .type = kind, .name = obj.name }) orelse {
             log.debug("{s} does not exist, creating", .{kind});
-            try db.exec(row[1], .{});
+            try db.exec(obj.sql);
             continue;
-        }
+        };
 
         // Check if object is the same
-        if (try db.get(bool, "SELECT sql = ? FROM sqlite_master WHERE type = ? AND name = ?", .{ row[1], kind, row[0] })) {
+        if (std.mem.eql(u8, curr.sql, obj.sql)) {
             log.debug("{s} already exists and is the same", .{kind});
             continue;
         }
@@ -57,62 +68,60 @@ fn migrateObjects(allocator: std.mem.Allocator, db: *sqlite.SQLite3, pristine: *
             log.debug("{s} already exists but is different, migrating", .{kind});
 
             // First, create a temp table with the new schema
-            const temp_sql = try std.fmt.allocPrint(allocator, "CREATE TABLE temp {s}", .{row[1][std.mem.indexOf(u8, row[1], "(").?..]});
-            defer allocator.free(temp_sql);
-            try db.exec(temp_sql, .{});
+            const temp_sql = try std.fmt.allocPrint(db.arena, "CREATE TABLE temp {s}", .{obj.sql[std.mem.indexOf(u8, obj.sql, "(").?..]});
+            try db.exec(temp_sql);
 
             // We want to copy data from the old table to the temp table so we need
             // to know which columns are common to both and we need to do it in a
             // new block so it gets deinitialized and we can then drop and rename
             // the temp table
             {
-                var cols = try db.query("SELECT GROUP_CONCAT(name) FROM (SELECT name FROM pragma_table_info(?) INTERSECT SELECT name FROM pragma_table_info('temp'))", .{row[0]});
-                defer cols.deinit();
+                var stmt = try db.prepare(dsl.raw("SELECT GROUP_CONCAT(name) FROM (SELECT name FROM pragma_table_info(?) INTERSECT SELECT name FROM pragma_table_info('temp'))", .{obj.name}));
+                defer stmt.deinit();
+
+                _ = try stmt.step();
+                const cols = try stmt.column([]const u8, 0);
 
                 // Copy data from old table to temp table
-                const copy_sql = try std.fmt.allocPrint(allocator, "INSERT INTO temp({0s}) SELECT {0s} FROM {1s}", .{ try cols.read([]const u8), row[0] });
-                defer allocator.free(copy_sql);
-                try db.exec(copy_sql, .{});
+                const copy_sql = try std.fmt.allocPrint(db.arena, "INSERT INTO temp({0s}) SELECT {0s} FROM {1s}", .{ cols, obj.name });
+                try db.exec(copy_sql);
             }
 
             // Drop old table
             log.debug("Dropping old table", .{});
-            const drop_sql = try std.fmt.allocPrint(allocator, "DROP TABLE {s}", .{row[0]});
-            defer allocator.free(drop_sql);
-            try db.exec(drop_sql, .{});
+            const drop_sql = try std.fmt.allocPrint(db.arena, "DROP TABLE {s}", .{obj.name});
+            try db.exec(drop_sql);
 
             // Rename temp table to old table
             log.debug("Renaming temp table", .{});
-            const rename_sql = try std.fmt.allocPrint(allocator, "ALTER TABLE temp RENAME TO {s}", .{row[0]});
-            defer allocator.free(rename_sql);
-            try db.exec(rename_sql, .{});
+            const rename_sql = try std.fmt.allocPrint(db.arena, "ALTER TABLE temp RENAME TO {s}", .{obj.name});
+            try db.exec(rename_sql);
         } else {
             log.debug("{s} already exists but is different, dropping and recreating", .{kind});
 
             // Drop old object
-            const drop_sql = try std.fmt.allocPrint(allocator, "DROP {s} {s}", .{ kind, row[0] });
-            defer allocator.free(drop_sql);
-            try db.exec(drop_sql, .{});
+            const drop_sql = try std.fmt.allocPrint(db.arena, "DROP {s} {s}", .{ kind, obj.name });
+            try db.exec(drop_sql);
 
             // Create new object
-            try db.exec(row[1], .{});
+            try db.exec(obj.sql);
         }
     }
 
     // Now we can check for extraneous objects and drop them
 
-    const all_names = try db.getString(allocator, "SELECT json_group_array(name) FROM sqlite_master WHERE type = ? AND name != 'sqlite_sequence'", .{kind});
-    defer allocator.free(all_names);
+    for (try db.findAll(objects)) |obj| {
+        if (try pristine.findBy(sqlite_master, .{ .type = kind, .name = obj.name }) == null) {
+            log.debug("-- Dropping extraneous {s} {s}", .{ kind, obj.name });
 
-    var extraneous = try pristine.query("SELECT json_each.value FROM json_each(?) WHERE json_each.value NOT IN (SELECT name FROM sqlite_master WHERE type = ?)", .{ all_names, kind });
-    defer extraneous.deinit();
-
-    while (try extraneous.readNext([]const u8)) |name| {
-        log.debug("-- Dropping extraneous {s} {s}", .{ kind, name });
-
-        const drop_sql = try std.fmt.allocPrint(allocator, "DROP {s} {s}", .{ kind, name });
-        defer allocator.free(drop_sql);
-
-        try db.exec(drop_sql, .{});
+            const drop_sql = try std.fmt.allocPrint(db.arena, "DROP {s} {s}", .{ kind, obj.name });
+            try db.exec(drop_sql);
+        }
     }
 }
+
+const sqlite_master = struct {
+    type: []const u8,
+    name: []const u8,
+    sql: []const u8,
+};
