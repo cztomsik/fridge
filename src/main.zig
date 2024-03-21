f5383e702ba10b55d971d3b46eb73d253a860b83
Kamil Tomsik <info@tomsik.cz>
2024-03-21 10:51:13 +0100

    re-export dsl.raw()

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ pub const SQLite3 = @import("sqlite.zig").SQLite3;
 pub const Pool = @import("pool.zig").Pool;
 pub const Session = @import("session.zig").Session;
 
+pub const raw = @import("dsl.zig").raw;
 pub const query = @import("dsl.zig").query;
 pub const insert = @import("dsl.zig").insert;
 pub const update = @import("dsl.zig").update;
