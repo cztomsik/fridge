fad42fb1d875441b8afd4e161e58ae580f88c4ef
Kamil Tomsik <info@tomsik.cz>
2024-08-12 16:48:54 +0200

    statement: refactor

--- src/statement.zig
+++ src/statement.zig
@@ -25,25 +25,13 @@ pub const Statement = extern struct {
     }
 
     pub fn bind(self: *Statement, index: usize, arg: anytype) !void {
-        try self.bindValue(index, try Value.from(arg, self.session.?.arena));
+        try self.vtable.bind(self.handle, index, try Value.from(arg, self.session.?.arena));
     }
 
     pub fn bindAll(self: *Statement, args: anytype) !void {
         inline for (0..args.len) |i| {
             try self.bind(i, args[i]);
         }
-    }
-
-    pub fn bindValue(self: *Statement, index: usize, arg: Value) !void {
-        try self.vtable.bind(self.handle, index, arg);
-    }
-
-    pub fn column(self: *Statement, comptime V: type, index: usize) !?V {
-        return switch (@typeInfo(V)) {
-            .Bool => (try self.vtable.column(self.handle, index, .bool)).bool,
-            .Int, .ComptimeInt => @intCast((try self.vtable.column(self.handle, index, .int)).int),
-            else => @panic("TODO"),
-        };
     }
 
     pub fn exec(self: *Statement) !void {
@@ -53,48 +41,61 @@ pub const Statement = extern struct {
     }
 
     pub fn value(self: *Statement, comptime V: type) !?V {
+        return if (try self.row(struct { V })) |r| r[0] else null;
+    }
+
+    pub fn row(self: *Statement, comptime R: type) !?R {
+        if (try self.next(R)) |r| {
+            try self.reset();
+            return r;
+        }
+
+        return null;
+    }
+
+    pub fn all(self: *Statement, comptime R: type) ![]const R {
+        var res = std.ArrayList(R).init(self.session.?.arena);
+
+        while (try self.next(R)) |r| {
+            try res.append(r);
+        }
+
+        return res.toOwnedSlice();
+    }
+
+    pub fn next(self: *Statement, comptime R: type) !?R {
         if (!try self.step()) {
             return null;
         }
 
-        const res = try self.column(V, 0);
-        try self.reset();
+        var res: R = undefined;
+
+        inline for (@typeInfo(@TypeOf(res)).Struct.fields, 0..) |f, i| {
+            @field(res, f.name) = try self.column(f.type, i);
+        }
+
         return res;
-    }
-
-    pub fn row(self: *Statement, comptime R: type) !?R {
-        defer self.reset();
-
-        @panic("TODO");
-        // return switch (try self.step()) {
-        //     .row => self.readRow(R),
-        //     .done => null,
-        // };
-    }
-
-    pub fn all(self: *Statement, comptime R: type) ![]const R {
-        _ = self;
-        @panic("TODO");
-        // var res = std.ArrayList(R).init(self.session.arena);
-
-        // while (try self.step() == .row) {
-        //     try res.append(
-        //         try self.readRow(R),
-        //     );
-        // }
-
-        // return res.toOwnedSlice();
     }
 
     fn step(self: *Statement) !bool {
         return self.vtable.step(self.handle);
     }
 
-    fn reset(self: *Statement) !void {
-        try self.vtable.reset(self.handle);
+    fn column(self: *Statement, comptime V: type, index: usize) !V {
+        if (comptime util.isString(V)) {
+            return (try self.vtable.column(self.handle, index, .string)).string;
+        }
+
+        return switch (@typeInfo(V)) {
+            .Bool => (try self.vtable.column(self.handle, index, .bool)).bool,
+            .Int, .ComptimeInt => @intCast((try self.vtable.column(self.handle, index, .int)).int),
+            else => {
+                @panic("TODO");
+            },
+        };
     }
 
-    fn finalize(self: *Statement) !void {
-        try self.vtable.finalize(self.handle);
+    pub fn reset(self: *Statement) !void {
+        try self.vtable.reset(self.handle);
     }
 };
