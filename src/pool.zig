34f7017ff1a9dc52190f0e720c8f79a210786450
Kamil Tomsik <info@tomsik.cz>
2024-08-19 11:37:42 +0200

    pool: wrap connections, add basic test for thread-safety

--- src/pool.zig
+++ src/pool.zig
@@ -1,17 +1,21 @@
 const std = @import("std");
+const util = @import("util.zig");
 const Connection = @import("connection.zig").Connection;
+const Statement = @import("statement.zig").Statement;
 const Session = @import("session.zig").Session;
 
 /// A simple connection pool. This is especially useful for web servers where
 /// each request needs its own "session" with separate transactions. The pool
 /// makes it easy to obtain a connection at the start of a request and release
 /// it at the end.
+///
+/// NOTE: This struct is self-referential, so it needs to be heap-allocated or
+/// pinned/never moved. Pinning is better, because the pool can still be
+/// referenced when `deinit()` is being called - it will just fail.
 pub const Pool = struct {
-    conns: std.ArrayList(Connection),
+    conns: std.ArrayList(PoolConn),
     factory: *const fn (opts: *const anyopaque) Error!Connection,
     opts: *const anyopaque,
-    max_count: usize,
-    count: usize = 0,
     mutex: std.Thread.Mutex = .{},
     wait: std.Thread.Condition = .{},
 
@@ -19,7 +23,7 @@ pub const Pool = struct {
 
     /// Initialize a connection pool with capacity for `max_count` connections
     /// which will be created using the provided driver-specific `options`.
-    pub fn init(comptime T: type, allocator: std.mem.Allocator, max_count: usize, options: *const T.Options) Pool {
+    pub fn init(comptime T: type, allocator: std.mem.Allocator, max_count: usize, options: *const T.Options) !Pool {
         const H = struct {
             fn open(opts: *const anyopaque) Error!Connection {
                 return Connection.open(T, @as(*const T.Options, @ptrCast(@alignCast(opts))).*);
@@ -27,10 +31,9 @@ pub const Pool = struct {
         };
 
         return .{
-            .conns = std.ArrayList(Connection).init(allocator),
-            .opts = options,
+            .conns = try std.ArrayList(PoolConn).initCapacity(allocator, max_count),
             .factory = H.open,
-            .max_count = max_count,
+            .opts = options,
         };
     }
 
@@ -49,90 +52,216 @@ pub const Pool = struct {
         // while waiting. On the other hand, it only happens once every time the
         // pool is empty, so maybe it's not worth the extra complexity.
         self.mutex.lock();
+        defer self.mutex.unlock();
 
         // TODO:
         // What's worse is that we might loose the connection (network error)
         // and then we should retry opening it. This is left for later.
 
         while (true) {
-            if (self.conns.popOrNull()) |conn| {
-                self.mutex.unlock();
-                return conn;
+            for (self.conns.items) |*pconn| {
+                if (pconn.available) {
+                    pconn.available = false;
+                    return util.upcast(pconn, Connection);
+                }
             }
 
-            if (self.count <= self.max_count) {
-                const conn = try self.factory(self.opts);
-                self.count += 1;
-
-                self.mutex.unlock();
-                return conn;
+            if (self.conns.items.len < self.conns.capacity) {
+                const pconn = self.conns.addOneAssumeCapacity();
+                pconn.* = .{ .pool = self, .conn = try self.factory(self.opts) };
+                return util.upcast(pconn, Connection);
             }
 
-            if (self.max_count == 0) {
-                self.mutex.unlock();
+            if (self.conns.capacity == 0) {
                 return error.PoolClosing;
             }
 
             self.wait.wait(&self.mutex);
             continue;
         }
-
-        return self.conns[0];
-    }
-
-    /// Put the connection back into the pool and notify any waiting threads.
-    pub fn releaseConnection(self: *Pool, conn: Connection) void {
-        self.mutex.lock();
-        self.conns.append(conn) catch unreachable;
-        self.mutex.unlock();
-        self.wait.signal();
     }
 
     /// Deinitialize the pool and close all connections.
     pub fn deinit(self: *Pool) void {
-        self.mutex.lock();
-        self.max_count = 0; // Make sure no new connections are created
-        const count = self.count; // Save before unlocking
-        self.mutex.unlock();
+        // Steal the list which will prevent any new connections (and deadlocks)
+        var pconns = brk: {
+            self.mutex.lock();
+            defer self.mutex.unlock();
+            break :brk self.conns.moveToUnmanaged();
+        };
 
-        // Reserve all connections and close them
-        for (0..count) |_| {
-            var conn = self.getConnection() catch unreachable;
-            conn.close();
+        // Notify all waiting threads that the pool is closing
+        self.wait.broadcast();
+
+        // Now we can aquire the mutex and continue without worying about deadlocks
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        // Gracefully close all connections
+        for (pconns.items) |*pconn| {
+            while (!pconn.available) {
+                // Wait up to 5 seconds and then close the connection forcefully
+                self.wait.timedWait(&self.mutex, 5 * std.time.ns_per_s) catch break;
+            }
+
+            pconn.conn.deinit();
         }
 
-        self.conns.deinit();
+        pconns.deinit(self.conns.allocator);
+    }
+};
+
+const PoolConn = struct {
+    pool: *Pool,
+    conn: Connection,
+    available: bool = false,
+
+    const Error = Connection.Error;
+
+    inline fn check(res: anytype) Error!@typeInfo(@TypeOf(res)).ErrorUnion.payload {
+        return res catch |e| switch (e) {
+            // TODO: check for error.BrokenPipe/ConnectionClosed and reconnect?
+            else => e,
+        };
+    }
+
+    pub fn execAll(self: *PoolConn, sql: []const u8) Error!void {
+        return check(self.conn.execAll(sql));
+    }
+
+    pub fn prepare(self: *PoolConn, sql: []const u8) Error!Statement {
+        return check(self.conn.prepare(sql));
+    }
+
+    pub fn rowsAffected(self: *PoolConn) Error!usize {
+        return check(self.conn.rowsAffected());
+    }
+
+    pub fn lastInsertRowId(self: *PoolConn) Error!i64 {
+        return check(self.conn.lastInsertRowId());
+    }
+
+    pub fn lastError(self: *PoolConn) []const u8 {
+        return self.conn.lastError();
+    }
+
+    pub fn deinit(self: *PoolConn) void {
+        self.pool.mutex.lock();
+
+        std.debug.assert(!self.available);
+        self.available = true;
+
+        self.pool.mutex.unlock();
+        self.pool.wait.signal();
     }
 };
 
 const t = std.testing;
 
-test {
-    var pool = Pool.init(@import("sqlite.zig").SQLite3, t.allocator, 3, &.{
-        .filename = ":memory:",
-    });
+const TestConn = struct {
+    id: u32,
+
+    pub const Options = void;
+    const Error = Connection.Error;
+
+    var created: std.atomic.Value(u32) = .{ .raw = 0 };
+    var destroyed: std.atomic.Value(u32) = .{ .raw = 0 };
+
+    pub fn open(_: void) !*TestConn {
+        const ptr = try std.testing.allocator.create(TestConn);
+        ptr.id = created.fetchAdd(1, .monotonic);
+        return ptr;
+    }
+
+    pub fn execAll(_: *TestConn, _: []const u8) Error!void {
+        unreachable;
+    }
+
+    pub fn prepare(_: *TestConn, _: []const u8) Error!Statement {
+        unreachable;
+    }
+
+    pub fn rowsAffected(_: *TestConn) Error!usize {
+        unreachable;
+    }
+
+    pub fn lastInsertRowId(_: *TestConn) Error!i64 {
+        unreachable;
+    }
+
+    pub fn lastError(_: *TestConn) []const u8 {
+        unreachable;
+    }
+
+    pub fn deinit(self: *TestConn) void {
+        std.testing.allocator.destroy(self);
+        _ = destroyed.fetchAdd(1, .monotonic);
+    }
+};
+
+test Pool {
+    var pool = try Pool.init(TestConn, t.allocator, 3, &{});
     defer pool.deinit();
 
-    const c1 = try pool.getConnection();
-    try t.expectEqual(1, pool.count);
-    try t.expectEqual(0, pool.conns.items.len);
-
-    pool.releaseConnection(c1);
-    try t.expectEqual(1, pool.count);
+    var c1 = try pool.getConnection();
     try t.expectEqual(1, pool.conns.items.len);
+    try t.expectEqual(false, pool.conns.items[0].available);
 
-    const c2 = try pool.getConnection();
-    try t.expectEqual(1, pool.count);
-    try t.expectEqual(0, pool.conns.items.len);
+    c1.deinit();
+    try t.expectEqual(1, pool.conns.items.len);
+    try t.expectEqual(true, pool.conns.items[0].available);
+
+    var c2 = try pool.getConnection();
+    try t.expectEqual(1, pool.conns.items.len);
+    try t.expectEqual(false, pool.conns.items[0].available);
     try t.expectEqual(c1.handle, c2.handle);
 
-    const c3 = try pool.getConnection();
-    try t.expectEqual(2, pool.count);
-    try t.expectEqual(0, pool.conns.items.len);
+    var c3 = try pool.getConnection();
+    try t.expectEqual(2, pool.conns.items.len);
+    try t.expectEqual(false, pool.conns.items[0].available);
+    try t.expectEqual(false, pool.conns.items[1].available);
     try t.expect(c1.handle != c3.handle);
 
-    pool.releaseConnection(c2);
-    pool.releaseConnection(c3);
-    try t.expectEqual(2, pool.count);
+    c2.deinit();
+    c3.deinit();
     try t.expectEqual(2, pool.conns.items.len);
+    try t.expectEqual(true, pool.conns.items[0].available);
+    try t.expectEqual(true, pool.conns.items[1].available);
+}
+
+const Runner = struct {
+    pool: *Pool,
+    thread: std.Thread,
+
+    fn run(self: *Runner) !void {
+        var count: usize = 0;
+        while (self.pool.getConnection()) |conn| : (count += 1) {
+            std.time.sleep(count % 10 * 10 * std.time.ns_per_ms);
+            conn.deinit();
+        } else |e| return switch (e) {
+            error.PoolClosing => {},
+            else => e,
+        };
+    }
+};
+
+test "Thread safety" {
+    var pool = try Pool.init(TestConn, t.allocator, 3, &{});
+    defer pool.deinit();
+
+    TestConn.created.store(0, .release);
+    TestConn.destroyed.store(0, .release);
+
+    var runners: [1000]Runner = undefined;
+    for (&runners) |*r| r.* = .{
+        .pool = &pool,
+        .thread = try std.Thread.spawn(.{}, Runner.run, .{r}),
+    };
+
+    std.time.sleep(500 * std.time.ns_per_ms);
+    pool.deinit();
+    for (runners) |r| r.thread.join();
+
+    try t.expectEqual(3, TestConn.created.load(.acquire));
+    try t.expectEqual(3, TestConn.destroyed.load(.acquire));
 }
